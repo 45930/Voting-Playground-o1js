30a02e87d2abf080bfde43d7217bc5f62a0c0204
Coby <contact@45930.xyz>
2023-10-16 22:28:20 -0400

    adding set election details script

---
+++ src/set_election_details.ts
@@ -0,0 +1,75 @@
+const ELECTION_DETAILS_KEY = 'B62qkwgSvxwZgXfwbYxwZRFZUD8HvsMke2jqz7iAaJ5ekpWHMWvJriK';
+
+import { Mina, PrivateKey } from 'o1js';
+import fs from 'fs/promises';
+import { Election, IpfsHash } from './Election.js';
+
+// check command line arg
+let deployAlias = process.argv[2];
+if (!deployAlias)
+  throw Error(`Missing <deployAlias> argument.
+
+Usage:
+node build/src/interact.js <deployAlias>
+`);
+Error.stackTraceLimit = 1000;
+
+// parse config and private key from file
+type Config = {
+  deployAliases: Record<
+    string,
+    {
+      url: string;
+      keyPath: string;
+      fee: string;
+      feepayerKeyPath: string;
+      feepayerAlias: string;
+    }
+  >;
+};
+let configJson: Config = JSON.parse(await fs.readFile('config.json', 'utf8'));
+let config = configJson.deployAliases[deployAlias];
+let feepayerKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
+  await fs.readFile(config.feepayerKeyPath, 'utf8')
+);
+
+let zkAppKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
+  await fs.readFile(config.keyPath, 'utf8')
+);
+
+let feepayerKey = PrivateKey.fromBase58(feepayerKeysBase58.privateKey);
+let zkAppKey = PrivateKey.fromBase58(zkAppKeysBase58.privateKey);
+
+// set up Mina instance and contract we interact with
+const Network = Mina.Network(config.url);
+const fee = Number(config.fee) * 1e9; // in nanomina (1 billion = 1.0 mina)
+Mina.setActiveInstance(Network);
+let feepayerAddress = feepayerKey.toPublicKey();
+let zkAppAddress = zkAppKey.toPublicKey();
+let zkApp = new Election(zkAppAddress);
+
+let sentTx;
+// compile the contract to create prover keys
+console.log('compile the contract...');
+await Election.compile();
+try {
+  // call update() and send transaction
+  console.log('build transaction and create proof...');
+  let tx = await Mina.transaction({ sender: feepayerAddress, fee }, () => {
+    zkApp.setElectionDetails(IpfsHash.fromString(ELECTION_DETAILS_KEY));
+  });
+  await tx.prove();
+  console.log('send transaction...');
+  sentTx = await tx.sign([feepayerKey]).send();
+} catch (err) {
+  console.log(err);
+}
+if (sentTx?.hash() !== undefined) {
+  console.log(`
+Success! Update transaction sent.
+
+Your smart contract state will be updated
+as soon as the transaction is included in a block:
+https://berkeley.minaexplorer.com/transaction/${sentTx.hash()}
+`);
+}
